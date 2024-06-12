e22268a1666ef8366c20fd259d0a5cd882a739f3
Castoxity Sama <sameerrajchal@gmail.com>
2024-06-12 17:25:35 +0545

    id change

--- src/js/script.js
+++ src/js/script.js
@@ -19,17 +19,17 @@ document.addEventListener('DOMContentLoaded', () => {
 document.querySelectorAll('.Buttons button').forEach(button => {
     button.addEventListener('click', () => {
         const id = button.id;
-        localStorage.setItem('clickedId', id); // Store clicked ID in localStorage
+        localStorage.setItem('clickedId', id); 
         fetch('dist/json/info.json')
             .then(response => response.json())
             .then(data => {
                 const userData = data.find(item => item.id === id);
                 if (userData) {
-                    localStorage.setItem('bgColor', userData["bg-color"]); // Store background color in localStorage
+                    localStorage.setItem('bgColor', userData["bg-color"]);
                 }
             })
             .then(() => {
-                window.location.href = 'user.html'; // Redirect to the user page
+                window.location.href = 'user.html'; 
             })
             .catch(error => {
                 console.error('Error fetching the JSON data:', error);
